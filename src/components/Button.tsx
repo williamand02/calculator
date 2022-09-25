8115738d404f162ff8c4a412e5c28e26f853a34a
william andrade de souza <williamstrayder@gmail.com>
2022-09-25 16:54:39 -0300

    :lipstick: feat: Estilizaçao do Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  MouseEvent,
+} from "react";
 
 import "./Button.scss";
 
 interface Props {
   label: string;
+  operation?: true;
+  double?: true;
+  triple?: true;
+  click?: (label: string) => void;
+  // size?: "double" | "triple";
 }
 
-const Button = ({ label }: Props) => {
-  return <button className="button">{label}</button>;
+const Button = ({ label, operation, double, triple, click }: Props) => {
+  return (
+    <button
+      type="button"
+      onClick={() => click && click(label)}
+      className={`
+    button
+    ${operation ? "operation" : ""}
+    ${double ? "double" : ""}
+    ${triple ? "triple" : ""}
+  `}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default Button;
